a3df92503783883e192e77cc003f9585189c1364
Mike Miller <mike@mikemiller.uk>
2025-03-29 09:09:22 +0100

    WIP - Saturday 29th Mar 2025 09:09 am

--- lib/posthog.ts
+++ lib/posthog.ts
@@ -1,7 +1,7 @@
 import { PostHog } from "posthog-node";
 
 const posthog = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  host: "https://app.posthog.com",
+  host: "https://eu.i.posthog.com",
 });
 
 export const identifyUser = async (
